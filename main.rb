f667d6069f0ddf3bb6f9f045a7abce868acad1c9
eduardo-salazar <esalazar922@gmail.com>
2016-11-30 01:13:03 +0800

    Creating json file

--- main.rb
+++ main.rb
@@ -1,12 +1,25 @@
 require 'open-uri'
 require 'pdf-reader'
+require 'json'
 
 io     = open('in/inputfile.pdf')
 reader = PDF::Reader.new(io)
+out = Hash.new
+
+def is_number? string
+  true if Float(string) rescue false
+end
 
 reader.pages.each do |page|
-  puts "Page #{page.text.lines.first.strip}"
-  page.text.lines.each do |line|
-    puts line.strip.split(' ')
+  unless page.number == 1
+    key = page.text.lines.first.strip
+    out[key] = Array.new unless out.key?(key)
+    page.text.delete('LEVEL 1 (1,080 words)').lines.each_with_index do |line,index|
+      out[key].push(line.strip) unless line.strip.empty? && is_number?(line.strip)
+    end
   end
+end
+
+File.open("vocabulary.json","w") do |f|
+  f.write(out.to_json)
 end
