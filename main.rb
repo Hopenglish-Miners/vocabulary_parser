e9180265815d583b10018e66de2c4be9e1837265
eduardo-salazar <esalazar922@gmail.com>
2016-11-30 16:42:42 +0800

    Creating json with vocabulary and adding postfix words

--- main.rb
+++ main.rb
@@ -6,20 +6,47 @@ io     = open('in/inputfile.pdf')
 reader = PDF::Reader.new(io)
 out = Hash.new
 
-def is_number? string
-  true if Float(string) rescue false
+def invalid?(string)
+  invalid = false
+  if string =~ /\d/ or string.strip.empty?
+    valid = true
+  end
+  valid
+end
+
+# Check if the word include parenthesis
+def postfix?(string)
+  !string.scan(/\(([^\)]+)\)/).empty?
+end
+
+# Check the parenthesis and create other word with postword
+def create_more_words(string)
+  # Get postfix
+  post = string.scan(/\(([^\)]+)\)/).last.first
+  word = string.sub(/\(([^\)]+)\)/,'')
+  [word,word+post]
 end
 
 reader.pages.each do |page|
   unless page.number == 1
     key = page.text.lines.first.strip
     out[key] = Array.new unless out.key?(key)
-    page.text.delete('LEVEL 1 (1,080 words)').lines.each_with_index do |line,index|
-      out[key].push(line.strip) unless line.strip.empty? && is_number?(line.strip)
+    page.text.split(/[\s|\/]/).each_with_index do |line,index|
+      line = line.strip
+      unless invalid?(line)
+        #Check if we can have to split the word in more.
+        # example: tactic(s) -> [tactic,tactics]
+        if postfix? line
+          out[key].push(*create_more_words(line))
+        else
+          out[key].push(line)
+        end
+      end
     end
   end
 end
 
+#puts JSON.pretty_generate(out)
 File.open("vocabulary.json","w") do |f|
   f.write(out.to_json)
 end
